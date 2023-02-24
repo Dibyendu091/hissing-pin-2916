650e67184532eebd0752f2e48cad67cdb64d31a6
Dibyendu Nath <112825458+Dibyendu091@users.noreply.github.com>
2023-02-24 13:22:26 +0530

    Done Day-03

--- cosmetics-fashion-house-clone/src/Pages/Women.jsx
+++ cosmetics-fashion-house-clone/src/Pages/Women.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import styles from './Women.module.css'
+import { Link } from 'react-router-dom'
 
 const Women = () => {
   return (
-    <div>Women</div>
+    <div className={styles.main}>
+      <Link>
+        <h1>Hello There</h1>
+      </Link>
+    </div>
   )
 }
 
